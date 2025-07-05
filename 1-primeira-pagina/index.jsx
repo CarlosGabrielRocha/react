1b32e864a6e0818dfa4d908ba506f5aed79797e9
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2025-07-05 09:11:50 -0300

    Start

---
+++ 1-primeira-pagina/index.jsx
@@ -0,0 +1,12 @@
+function PrimaryButton() { /* Componentes são escritos em PascalCase */
+    return <button>Meu botão</button>
+}
+
+function container() {
+    return (
+        <div className="">
+            <h1></h1>
+            <p></p>
+        </div>
+    )
+}
---
+++ 4-ex01/src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <main>
+      <img src="/react.png" alt="react"/>
+      <h1>React</h1>
+        <p>A biblioteca para interfaces de usuário web e nativas</p>
+        <div>
+          <button>Aprenda React</button>
+          <button>Referência da API</button>
+        </div>
+        <hr/>
+        <h1>Crie interfaces de usuário de componentes</h1>
+        <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes.</p>
+        <hr/>
+        <h1>Escreva componentes com código e marcação</h1>
+        <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada JSX. É uma extensão da sintaxe JavaScript popularizada pelo React.</p>
+        <hr/>
+        <h1>Próximos Passos</h1>
+        <ul>
+          <li>Uso de dados dinâmicos no JSX</li>
+          <li>Criação de novos componentes</li>
+          <li>Estilização de componentes</li>
+          <li>Reutilização de componentes</li>
+          <li>Uso de props e children</li>
+          <li>Uso de eventos de JavaScript</li>
+        </ul>
+    </main>
+  )
+}
+
+export default App
