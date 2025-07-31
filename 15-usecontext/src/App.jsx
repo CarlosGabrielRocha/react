a77307fc9e061288ed9e61baf86602b6590a54b3
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2025-07-31 08:28:48 -0300

    useEffect e useCommit

---
+++ 15-usecontext/src/App.jsx
@@ -0,0 +1,24 @@
+import Container from "./components/Container"
+import Header from "./components/Header"
+import UserContext from "./contexts/UserContext"
+
+export default function App() {
+  const user = {
+    name: "João",
+    email: "Joao@email.com"
+  }
+  return (
+    <>
+    <UserContext.Provider value={user}>
+      <Header />
+      <h1>Usando contexto</h1>
+      <Container>
+        <p>Este é o nosso app com contexto</p>
+      </Container>
+    </UserContext.Provider>
+    <div>
+      {/* Não há acesso ao contexto */}
+    </div>
+    </>
+  )
+}
